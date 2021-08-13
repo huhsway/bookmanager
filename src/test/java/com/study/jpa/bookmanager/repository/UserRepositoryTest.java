5a4092dcb5d849327649bdbdece86d6db7fcc00b
seunghoe_heo <seunghoe_heo@tmax.co.kr>
2021-08-13 16:29:08 +0900

    Repository Interface 메소드 실습

--- src/test/java/com/study/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/study/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,11 +1,17 @@
 package com.study.jpa.bookmanager.repository;
 
 import com.study.jpa.bookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Sort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
 
 @SpringBootTest // 스프링 컨텍스트를 로딩을 해서 테스트에 활용
 class UserRepositoryTest {
@@ -14,8 +20,13 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        userRepository.save(new User());
+        User user = new User();
+        user.setEmail("slow");
 
-        System.out.println(">>> " + userRepository.findAll());
+        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("email", contains());
+        Example<User> example = Example.of(user, matcher);
+
+        userRepository.findAll(example).forEach(System.out::println);
+
     }
 }
